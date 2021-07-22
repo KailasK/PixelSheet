1614d68c299b726e1365efadfe316cd3fe026450
Kailas Krivanka <38896831+kailask@users.noreply.github.com>
2021-07-21 22:27:36 -0700

    reorganized files and added new readme

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # PixelSheet
 
-This repo contains the backend for a google sheets extension that allows a requested image to be inserted into a google sheet. The image is divided up into averaged color values for each cell in the sheet. The background color of each cell is then set to the appropriate color.
+PixelSheet is a Google Sheets addon that provides an easy way to automatically create pixel art in a Google Sheet. It works by averaging color values across the image and recoloring the cells of the Sheet.
 
-</br>
-<img src="Capture.JPG">
+## Overview
 
+The app is separated into a frontend Google Sheets addon and a backend webservice due to limitations on accessing external resources and third-party libraries imposed by Google AppsScript. The PixelSheet [frontend](addon/) consists of a Google Sheets addon written in AppsScript. The [backend](service/) consists of a Node.js application using Express.js to parse images. The backend _was_ hosted on RedHat OpenShift.
+
+<p align="center">
+   <img src="assets/architecture.png" alt="Architecture"/> 
+</p>
+
+## Getting Started
+
+### Dependencies
+
+* `npm`
+* Google Account
+
+### Before Using
+
+* The backend Node application needs to be publicly hosted.
+* The addon features a hardcoded endpoint that needs to be updated.
+* Many dependencies are out of date and have vulnerabilities.
+
+### Using
+
+To use PixelSheet the files in the [addon](addon/) folder need to be attached to a Google AppsScript project associated with your Google Account. For details on how to do this see [here](https://developers.google.com/apps-script/overview). Then you can enable it on your account and use it with any Google Sheet.
---
+++ service/index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+var myParser = require("body-parser");
+var getPixels = require("get-pixels");
+var ndarray = require("ndarray");
+
+var img;
+var map = [];
+
+app.use(myParser.json())
+
+app.post('/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    getPixels(req.body.url, function (err, pixels) {
+        if (err || req.body.width <= 1) {
+            console.log(err);
+            res.send(JSON.stringify({ error: err }))
+            return;
+        } else {
+            console.log(pixels.shape.slice());
+            img = pixels;
+            map = [];
+            var s = (img.shape[0] - (img.shape[0] % req.body.width)) / req.body.width;
+            var c = img.lo(img.shape[0] % req.body.width, img.shape[1] % s, img.shape[2]);
+            genMap(s, c);
+            res.send(JSON.stringify({ data: map }))
+        }
+    });
+})
+
+function genMap(s, c) {
+    var working;
+    for (var i = 0; i < c.shape[0]; i += s) {
+        var arr = [];
+        for (var j = 0; j < c.shape[1]; j += s) {
+            var p = {};
+            for (var k = 0; k < img.shape[2]; k++) {
+                working = c.lo(i, j, k).hi(s, s, 1);
+                var avg = 0;
+                for (var a = 0; a < s - 1; a++) {
+                    for (var b = 0; b < s - 1; b++) {
+                        avg += working.get(a, b, 0);
+                    }
+                }
+                avg = parseInt(avg / ((s - 1) * (s - 1)));
+                switch (k) {
+                    case 0:
+                        p.r = avg;
+                        break;
+                    case 1:
+                        p.g = avg;
+                        break;
+                    case 2:
+                        p.b = avg;
+                        break;
+                    case 3:
+                        p.a = avg;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            arr.push(p);
+        }
+        map.push(arr);
+    }
+}
+
+app.listen(8080, () => console.log('listening on port 8080'))
